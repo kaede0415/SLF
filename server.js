a97613a5977de0b5c6cb0702fd17957a30524dac
Glitch (kamui-9) <none>
2022-02-01 00:54:54 +0000

    🍂⛰ Checkpoint ./server.js:29682831/3560 ./package.json:29682831/1382

--- server.js
+++ server.js
@@ -1,61 +1,39 @@
 const http = require('http');
 const querystring = require('querystring');
-const discord = require('discord.js');
+const discord = require('discord.js-selfbot');
 const client = new discord.Client();
 
 http.createServer(function(req, res){
-  if (req.method == 'POST'){
-    var data = "";
-    req.on('data', function(chunk){
-      data += chunk;
-    });
-    req.on('end', function(){
-      if(!data){
-        res.end("No post data");
-        return;
-      }
-      var dataObject = querystring.parse(data);
-      console.log("post:" + dataObject.type);
-      if(dataObject.type == "wake"){
-        console.log("Woke up in post");
+ if (req.method == 'POST'){
+   var data = "";
+   req.on('data', function(chunk){
+     data += chunk;
+   });
+   req.on('end', function(){
+     if(!data){
+        console.log("No post data");
         res.end();
         return;
-      }
-      res.end();
-    });
-  }
-  else if (req.method == 'GET'){
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Discord Bot is active now\n');
-  }
+     }
+     var dataObject = querystring.parse(data);
+     console.log("post:" + dataObject.type);
+     if(dataObject.type == "wake"){
+       console.log("Woke up in post");
+       res.end();
+       return;
+     }
+     res.end();
+   });
+ }
+ else if (req.method == 'GET'){
+   res.writeHead(200, {'Content-Type': 'text/plain'});
+   res.end('Discord Bot is active now\n');
+ }
 }).listen(3000);
 
-client.on('ready', message =>{
-  console.log('Bot準備完了～');
-  client.user.setPresence({ activity: { name: 'げーむ' } });
+client.on("ready", () => {
+console.log(`${client.user.tag} is ready!`)
 });
-
-client.on('message', message =>{
-  if (message.author.id == client.user.id || message.author.bot){
-    return;
-  }
-  if(message.isMemberMentioned(client.user)){
-    sendReply(message, "呼びましたか？");
-    return;
-  }
-  if (message.content.match(/にゃ～ん|にゃーん/)){
-    let text = "にゃ～ん";
-    sendMsg(message.channel.id, text);
-    return;
-  }
-});
-
-if(process.env.DISCORD_BOT_TOKEN == undefined){
- console.log('DISCORD_BOT_TOKENが設定されていません。');
- process.exit(0);
-}
-
-client.login( process.env.DISCORD_BOT_TOKEN );
 
 function sendReply(message, text){
   message.reply(text)
@@ -68,3 +46,21 @@ function sendMsg(channelId, text, option={}){
     .then(console.log("メッセージ送信: " + text + JSON.stringify(option)))
     .catch(console.error);
 }
+
+client.on('message', async message => {
+  if(message.author.bot){
+  return;
+}
+  if(message.author.id === "707459939446161438" || message.author.id === "759001587422462015"){
+  if (message.content.toLowerCase().startsWith("!9say")) {
+   const [name, ...args] = message.content.slice(3).split("y ");
+  message.channel.send(args[0])
+    }
+  if (message.content.toLowerCase().startsWith("!allsay")) {
+   const [name, ...args] = message.content.slice(3).split("y ");
+  message.channel.send(args[0])
+    }
+  }
+});
+
+client.login("ODk2OTc3NTU2NjUyMjk0MTU1.YfhyYA.UPWOzLvni8BL5YOYhJaRKhgJqbs")
