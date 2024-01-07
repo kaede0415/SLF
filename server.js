07a75c60c8a0d2ecdd864701fee4c06599e91339
Glitch (kamui-9) <none>
2024-01-07 01:50:28 +0000

    📊🍡 Checkpoint ./server.js:60183201/3

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const querystring = require('querystring');
-const discord = require('discord.js-self');
+const discord = require('discord.js-selfbot');
 const client = new discord.Client();
 const target_ch_id = "1185577069195821096"
 const log_ch_id = "1191001968026603560" 
